a666d93a73b756a24092f519cf5c2acc2ab200dc
Bereket Abera <bereket5042@gmail.com>
2021-02-03 20:31:40 +0000

    Fix 5th n 6th solution

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,4 +18,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", (int) sizeof(l));
 	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(ll));
 	printf("Size of a float: %d byte(s)\n", (int) sizeof(f));
+	return (0);
 }

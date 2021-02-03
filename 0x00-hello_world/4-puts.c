9412e9d882e01e24ead4b760f7e43015ee4a5318
Bereket Abera <bereket5042@gmail.com>
2021-02-03 19:26:25 +0000

    Fix solution 4 phase 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+/*
+ * prints a string to the standard output
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }

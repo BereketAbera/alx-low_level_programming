bfb8dfd32cac903e78f80dc26d34419b57c18589
Bereket Abera <bereket5042@gmail.com>
2021-02-11 17:34:45 +0000

    Add solution 1 and 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - multiply to numbers
+ * @n: the first number
+ * @m: the second number
+ *
+ * Return: the multiplication of the two numbers
+ */
+int mul(int n, int m)
+{
+	return (n * m);
+}
+

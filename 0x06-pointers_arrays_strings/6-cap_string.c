49b0ff023f38f0a38af78afddd3b5c72c7ee7ab3
Bereket Abera <bereket5042@gmail.com>
2021-02-18 17:33:31 +0000

    Fix solution 6 pr 0x06 phase 1

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,6 +13,12 @@ char *cap_string(char *s)
 
 	while (s[l] != '\0')
 	{
+		if (cap && s[l] >= 97 && s[l] < 97 + 26)
+		{
+			s[l] = s[l] - 32;
+			cap = 0;
+		} else
+			cap = 0;
 		for (i = 0; i < 13; i++)
 		{
 			if (s[l] == separators[i])
@@ -21,12 +27,6 @@ char *cap_string(char *s)
 				break;
 			}
 		}
-		if (cap && s[l] >= 97 && s[l] < 97 + 26)
-		{
-			s[l] = s[l] - 32;
-			cap = 0;
-		} else if (s[l] >= 65 && s[l] <= 90)
-			cap = 0;
 		l++;
 	}
 	return (s);

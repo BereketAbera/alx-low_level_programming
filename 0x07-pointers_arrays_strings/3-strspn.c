dcf0f2d1947895dd229c38b450f36621216aa318
Bek <bereket5042@gmail.com>
2021-02-26 21:47:57 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,4 @@
-char *removeChar(char s[], int i);
+#include "holberton.h"
 
 /**
  * _strspn - find number of charaters not found in accept
@@ -9,62 +9,20 @@ char *removeChar(char s[], int i);
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int l = 0;
-	int i, found;
-	int ln = 0;
-
-	while (s[ln] != '\0')
-		ln++;
-	while (s[l] != '\0')
-	{
-		i = 0;
-		found = 0;
-		if (accept[i] == '\0')
-			break;
-		while (accept[i] != '\0' && i <= ln)
-		{
-			if (accept[i] == s[l])
-			{
-				found = 1;
-				break;
-			}
-			i++;
-		}
-		if (found)
-		{
-			accept = removeChar(accept, i);
-		}
-		l++;
-	}
-	return (l);
-}
-
-
-/**
- * removeChar - romoves char
- * @s: a string to remove char from
- * @i: the index to remove the char from
- *
- * Return: the string with the removed char
- */
-char *removeChar(char *s, int i)
-{
+	int i = 0;
 	int j = 0;
-	int temp = i;
-	static char t[100];
-
-	i = 0;
-	while (s[j] != '\0')
+	int count = 0;
+	
+	while (*(s + i) != '\0'))
 	{
-		if (i == temp)
+		while (*(accept + j))
 		{
+			if (*(s + i) == *(accept + j))
+				count++;
 			j++;
-			continue;
 		}
-		t[i] = s[j];
-		j++;
 		i++;
 	}
-	t[i] = '\0';
-	return (t);
+	
+	return (count);
 }

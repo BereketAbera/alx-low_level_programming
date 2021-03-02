107bd78837588b34ceaca7f031c5b470fa8d582f
Bereket Abera <bereket5042@gmail.com>
2021-03-02 17:06:16 +0000

    Add solution 3 pr 0x0B

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i;
 
-	if (!size)
+	if (size <= 0)
 		return (0);
 
 	array = malloc(size);
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+
+/**
+ * str_concat - concat two strings
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: a pointer to the new concatinated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j;
+	int l = 0;
+	int l2 = 0;
+	int total = 0;
+	char *s1s2;
+
+	if (s1)
+	{
+		while (s1[l] != '\0')
+		{
+			l++;
+			total++;
+		}
+	}
+	if (s2)
+	{
+		while (s2[l2] != '\0')
+		{
+			l2++;
+			total++;
+		}
+	}
+	if (total == 0)
+		return (0);
+
+	s1s2 = malloc(total);
+	if (!s1s2)
+		return (0);
+	for (i = 0; i < l; i++)
+		s1s2[i] = s1[i];
+
+	for (j = 0; j < l2; j++)
+		s1s2[i + j] = s2[j];
+
+	s1s2[i + j] = '\0';
+	return (s1s2);
+}

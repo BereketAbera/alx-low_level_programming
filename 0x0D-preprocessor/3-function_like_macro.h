48e003d7dc5e6ad2a3cce3c7b49f2364fdfdaf71
Bereket Abera <bereket5042@gmail.com>
2021-03-08 18:14:29 +0000

    Fix solution 3 & 4 pr 0x0D

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,4 @@
-#ifndef ABS
+#ifndef ABSOLUTE
+#define ABSOLUTE
 #define ABS(x) ((x) >= 0 ? (x) : ((x) * -1))
 #endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,3 +1,4 @@
-#ifndef SUM
+#ifndef SUMTWO
+#define SUMTWO
 #define SUM(x, y) (x + y)
 #endif

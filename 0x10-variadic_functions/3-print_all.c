2aa2eac5473615df26fd101b0722204f6621c020
Bereket Abera <bereket5042@gmail.com>
2021-03-11 19:51:34 +0000

    Fix solution 3 pr 0x10

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,7 +11,7 @@
 void print_all(const char * const format, ...)
 {
 	va_list values;
-	int i = 0, j = 0, valid = 0;
+	int i = 0, j = 0;
 	char *formatchars = "cifs";
 	char *printfformats[] = {"%c", "%d", "%f", "%s"};
 	void *currValue;
@@ -19,30 +19,25 @@ void print_all(const char * const format, ...)
 	va_start(values, format);
 	while (format[i] != '\0')
 	{
-		valid = 0;
 		j = 0;
 		while (j < 4)
 		{
+			if (formatchars[j] == format[i] && format[i + 1] == '\0')
+			{
+				currValue = va_arg(values, void *);
+				printf(printfformats[j], currValue);
+				break;
+			}
+
 			if (formatchars[j] == format[i])
 			{
-				valid = 1;
 				currValue = va_arg(values, void *);
+				printf(printfformats[j], currValue);
+				printf(", ");
 				break;
 			}
 			j++;
 		}
-		if (valid == 1 && format[i + 1] != '\0' && (currValue != NULL || currValue == 0))
-		{
-			printf(printfformats[j], currValue);
-			printf(", ");
-		} else if (valid == 1 && (currValue != NULL || currValue == 0))
-			printf(printfformats[j], currValue);
-		else if (valid == 1 && format[i + 1] != '\0')
-		{
-			printf(printfformats[j], "(nil)");
-			printf(", ");
-		} else if (valid == 1)
-			printf(printfformats[j], "(nil)");
 		i++;
 	}
 	printf("\n");
